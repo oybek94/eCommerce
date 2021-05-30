74e83369d137eb7acd718e8929efa388f15ab153
oybek94 <oybek94@bk.ru>
2021-05-30 18:23:25 +0500

    burger is working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.querySelector('.top-header__burger').addEventListener('click',()=> {
    document.querySelector('.top-header__burger').classList.toggle('active');
-   document.querySelector('.top-header').classList.toggle('active');
+   document.querySelector('.top-menu').classList.toggle('open');
    document.querySelector('body').classList.toggle('lock');
 });
 
