aedae398e04f0556a788240bb7c49e42e067c452
oybek94 <oybek94@bk.ru>
2021-06-02 16:55:53 +0500

    testimonial section complate

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-   $("#lightSlider").lightSlider({
+   $('#lightSlider').lightSlider({
+       item:4,
+       loop:true,
+       slideMove:2,
+       easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+       speed:600,
+       responsive : [
+           {
+               breakpoint:1400,
+               settings: {
+                   item:3,
+                   slideMove:1,
+                   slideMargin:6,
+                 }
+           },
+           {
+               breakpoint:1070,
+               settings: {
+                   item:2,
+                   slideMove:1
+                 }
+           },
+           {
+            breakpoint:710,
+            settings: {
+                item:1,
+                slideMove:1
+              }
+        }
 
-      item:4,
-      loop: true,
-      autoWidth: false,
-   }); 
+       ]
+   });  
  });
