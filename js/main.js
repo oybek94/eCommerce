976596b696e4ffd4af77fc15a6c0593e63ac43db
oybek94 <oybek94@bk.ru>
2021-06-12 14:51:59 +0500

    list/grid view WIP

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 document.querySelector('.top-header__burger').addEventListener('click',()=> {
    document.querySelector('.top-header__burger').classList.toggle('active');
    document.querySelector('.top-menu').classList.toggle('open');
-   //document.querySelector('body').classList.toggle('lock');
 });
 
+document.querySelector('.gird').addEventListener('click',()=> {
+   document.querySelector('.card__list').classList.remove('column');
+});
+
+document.querySelector('.list').addEventListener('click',()=> {
+   document.querySelector('.card__list').classList.add('column');
+});
